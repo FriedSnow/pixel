a144ca8d6417d08cbbcbda5b99bc718c2bdb96d2
Alexander Kh <saneknv05@gmail.com>
2023-05-22 09:46:33 +0300

    upd

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -15,7 +15,7 @@ import android.widget.EditText;
 import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
-    public int size;
+    static int size;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
